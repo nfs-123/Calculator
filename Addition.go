6216ef18e16abad1cb35314399bbe676e4c91bac
Nawaz Shaikh <nawazs@mkcl.org>
2023-02-05 15:42:35 +0530

    Added divide function

--- Addition.go
+++ Addition.go
@@ -1,5 +1,16 @@
 package calc
 
+import "errors"
+
 func Sum(no1, no2 int) int {
 	return no1 + no2
+}
+
+func Divide(no1, no2 int) (error, int) {
+	if no2 == 0 {
+		return errors.New("Can't divide by zero"), 0
+	} else {
+		return nil, no1 / no2
+	}
+
 }
